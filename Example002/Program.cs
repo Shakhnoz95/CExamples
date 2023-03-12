1f3c3692224bb1e41672da090b49b2a25108b126
shakhnoz95 <shakhnoz9596@gmail.com>
2023-03-12 16:48:05 +0300

    example002 added

--- Example002/Program.cs
+++ Example002/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите имя пользователя:");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша") //ToLower() преобразует всё в нижний регистрсдуфк
+{
+Console.WriteLine("Ура!Это же Маша");
+}
+
+else
+{
+Console.Write("Привет, ");
+Console.Write(username);
+}
+
